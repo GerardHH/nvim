c4dc8565b491b7effd467b20d41cf61f4956aa4e
gerard <gerard@pop-os.localdomain>
2023-09-16 20:30:40 +0200

    lazy.lua: Ensure colorscheme, disable notify for checker and disable notify for change_detection

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -11,4 +11,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({import = "user.plugins"})
+require("lazy").setup({import = "user.plugins"}, {
+    install = {
+        colorscheme = { "melange" },
+    },
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        notify = false,
+    },
+})

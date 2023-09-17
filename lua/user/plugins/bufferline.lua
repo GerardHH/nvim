75cb4c4153a52de12823449479eba6c74a2453da
gerard <gerard@pop-os.localdomain>
2023-09-17 16:28:42 +0200

    bufferline.lua: Add better close commands

--- lua/user/plugins/bufferline.lua
+++ lua/user/plugins/bufferline.lua
@@ -1,11 +1,11 @@
 return {
 	"akinsho/bufferline.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = { "nvim-tree/nvim-web-devicons", "moll/vim-bbye" },
 	version = "*",
 	opts = {
 		options = {
-			mode = "tabs",
-			separator_style = "slant",
+			close_command = "Bdelete! %d",
+			right_mouse_command = "Bdelete! %d",
 		},
 	},
 }

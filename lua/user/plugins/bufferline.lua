470df50780d9486b05b746810f7500cf1b750b15
Gerard Heshusius <gh.heshsusius@gmail.com>
2023-09-20 18:33:43 +0200

    bufferline.lua: Set which-key mappings

--- lua/user/plugins/bufferline.lua
+++ lua/user/plugins/bufferline.lua
@@ -1,11 +1,25 @@
 return {
 	"akinsho/bufferline.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", "moll/vim-bbye" },
+	dependencies = { "nvim-tree/nvim-web-devicons", "moll/vim-bbye", "folke/which-key.nvim" },
 	version = "*",
-	opts = {
-		options = {
-			close_command = "Bdelete! %d",
-			right_mouse_command = "Bdelete! %d",
-		},
-	},
+	config = function()
+		require("bufferline").setup({
+			options = {
+				close_command = "Bdelete! %d",
+				right_mouse_command = "Bdelete! %d",
+			},
+		})
+
+		local wk = require("which-key")
+		wk.register({
+			b = {
+				name = "Buffer",
+				c = { "<cmd>BufferLinePickClose<cr>", "Buffer pick Close" },
+				l = { "<cmd>BufferLineCloseLeft<cr>", "Buffer close Left" },
+				o = { "<cmd>BufferLineCloseOthers<cr>", "Buffer close Others" },
+				p = { "<cmd>BufferLinePick<cr>", "Buffer Pick open" },
+				r = { "<cmd>BufferLineCloseRight<cr>", "Buffer close Right" },
+			},
+		}, { prefix = "<leader>" })
+	end,
 }

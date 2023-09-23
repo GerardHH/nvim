4759b81ac3ec05a7085bf0f46b42d13235fe58fa
Gerard Heshusius <gh.heshsusius@gmail.com>
2023-09-23 18:10:02 +0200

    mason.lua: Add descriptive comment to installed servers

--- lua/user/plugins/lsp/mason.lua
+++ lua/user/plugins/lsp/mason.lua
@@ -28,8 +28,8 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
-				"lua_ls",
-				"bashls",
+				"lua_ls", -- lua
+				"bashls", -- shell
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed

f22f551c33a227297e2e22d0df6ea10c3882a351
Gerard Heshusius <gh.heshsusius@gmail.com>
2023-09-23 18:09:12 +0200

    bufferline.lua: Use correct capitalization

--- lua/user/plugins/bufferline.lua
+++ lua/user/plugins/bufferline.lua
@@ -14,11 +14,11 @@ return {
 		wk.register({
 			b = {
 				name = "Buffer",
-				c = { "<cmd>BufferLinePickClose<cr>", "Buffer pick Close" },
-				l = { "<cmd>BufferLineCloseLeft<cr>", "Buffer close Left" },
-				o = { "<cmd>BufferLineCloseOthers<cr>", "Buffer close Others" },
-				p = { "<cmd>BufferLinePick<cr>", "Buffer Pick open" },
-				r = { "<cmd>BufferLineCloseRight<cr>", "Buffer close Right" },
+				c = { "<cmd>BufferLinePickClose<cr>", "Buffer pick close" },
+				l = { "<cmd>BufferLineCloseLeft<cr>", "Buffer close left" },
+				o = { "<cmd>BufferLineCloseOthers<cr>", "Buffer close others" },
+				p = { "<cmd>BufferLinePick<cr>", "Buffer pick open" },
+				r = { "<cmd>BufferLineCloseRight<cr>", "Buffer close right" },
 			},
 		}, { prefix = "<leader>" })
 	end,

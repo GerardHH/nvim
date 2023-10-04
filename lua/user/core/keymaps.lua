0ebdda3fec18ea23db369d339bc98f5628c2155e
Gerard Heshusius <gHeshusius@lely.com>
2023-10-04 20:34:09 +0200

    core: keymaps.lua: Add desc to every keymap

--- lua/user/core/keymaps.lua
+++ lua/user/core/keymaps.lua
@@ -6,19 +6,17 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Normal --
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", {})
-keymap("n", "<C-j>", "<C-w>j", {})
-keymap("n", "<C-k>", "<C-w>k", {})
-keymap("n", "<C-l>", "<C-w>l", {})
+keymap("n", "<C-h>", "<C-w>h", { desc = "Navigate to left window" })
+keymap("n", "<C-j>", "<C-w>j", { desc = "Navigate to down window" })
+keymap("n", "<C-k>", "<C-w>k", { desc = "Navigate to up window" })
+keymap("n", "<C-l>", "<C-w>l", { desc = "Navigate to right window" })
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", {})
-keymap("n", "<C-Down>", ":resize +2<CR>", {})
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", {})
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", {})
+keymap("n", "<C-Up>", ":resize +2<CR>", { desc = "Expand current view horizontally" })
+keymap("n", "<C-Down>", ":resize -2<CR>", { desc = "Shrink current view horizontally" })
+keymap("n", "<C-Left>", ":vertical resize +2<CR>", { desc = "Expand current view vertically" })
+keymap("n", "<C-Right>", ":vertical resize -2<CR>", { desc = "Shrink current view vertically" })
 
--- Buffers
 keymap("n", "<S-l>", ":bnext<CR>", { desc = "Next buffer" })
 keymap("n", "<S-h>", ":bprevious<CR>", { desc = "Previous buffer" })
 
@@ -33,31 +31,25 @@ keymap("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
--- Move text up and down
-keymap("n", "<A-j>", ":m .+1<CR>==", {})
-keymap("n", "<A-k>", ":m .-2<CR>==", {})
-
--- Meta actions
-keymap("n", "<leader>ch", ":checkhealth<CR>", { desc = "Check Health" })
+keymap("n", "<A-j>", ":m .+1<CR>==", { desc = "Move text on line down" })
+keymap("n", "<A-k>", ":m .-2<CR>==", { desc = "Move text on line up" })
 
 -- Insert --
--- Press jk fast to exit insert mode
-keymap("i", "jk", "<ESC>", {})
-keymap("i", "kj", "<ESC>", {})
+keymap("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+keymap("i", "kj", "<ESC>", { desc = "Exit insert mode" })
 
 -- Visual --
--- Stay in indent mode
-keymap("v", "<", "<gv^", {})
-keymap("v", ">", ">gv^", {})
+keymap("v", "<", "<gv^", { desc = "Indent selected text to the left" })
+keymap("v", ">", ">gv^", { desc = "Indent selected text to the right" })
 
--- Move text up and down
-keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", {})
-keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", {})
-keymap("v", "p", '"_dP', {})
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selected text up" })
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selected text down" })
+
+keymap("v", "p", '"_dP', { desc = "Replace selection with buffer" })
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":m '>+1<CR>gv=gv", {})
-keymap("x", "K", ":m '<-2<CR>gv=gv", {})
-keymap("x", "<A-j>", ":m '>+1<CR>gv=gv", {})
-keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", {})
+keymap("x", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected text up" })
+keymap("x", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected text down" })
+keymap("x", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selected text up" })
+keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selected text down" })

06d5171c1365d859efbecaa7aa741de6f09496b5
Gerard Heshusius <gHeshusius@lely.com>
2023-10-08 12:15:49 +0200

    colorscheme.lua: Add catppuccin and enable mocha

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -1,10 +1,23 @@
 return {
 	{
 		"savq/melange-nvim",
+		name = "malange",
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd.colorscheme("melange")
+			-- vim.cmd.colorscheme("melange")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+			})
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 }

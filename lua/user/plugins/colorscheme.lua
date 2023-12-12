e66e2410e2db46016504681d7d33cb83be980d5c
Gerard Heshusius <gHeshusius@lely.com>
2023-12-12 12:58:17 +0100

    colorscheme: Remove melange

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -1,14 +1,5 @@
 return {
 	{
-		"savq/melange-nvim",
-		name = "malange",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			-- vim.cmd.colorscheme("melange")
-		end,
-	},
-	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000, -- make sure to load this before all the other start plugins

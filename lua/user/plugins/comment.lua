525bd7cf3b2a6656ed2b63e57d96a0bdf26f2042
Gerard Heshusius <gHeshusius@lely.com>
2024-01-03 14:31:29 +0100

    comment: Add tree-sitter hook

--- lua/user/plugins/comment.lua
+++ lua/user/plugins/comment.lua
@@ -1,5 +1,14 @@
 return {
 	"numToStr/Comment.nvim",
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
 	event = { "BufReadPre", "BufNewFile" },
-	config = true,
+	config = function()
+		---@diagnostic disable:missing-fields
+		require("Comment").setup({
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		})
+		---@diagnostic enable:missing-fields
+	end,
 }

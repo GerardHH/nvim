efa6923e1488ed566dde186cdc86e897ab3351f6
Gerard Heshusius <gHeshusius@lely.com>
2024-01-12 16:09:24 +0100

    flash: Self define fFtT and instead of leader keys use sS

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,23 +1,95 @@
 return {
 	"folke/flash.nvim",
-	event = "VeryLazy",
 	lazy = true,
 	keys = {
 		{
-			"<leader>ff",
-			mode = { "n", "x", "o" },
+			"s",
 			function()
 				require("flash").jump()
 			end,
+			mode = { "n", "x", "o" },
 			desc = "Flash flash",
 		},
 		{
-			"<leader>fF",
-			mode = { "n", "x", "o" },
+			"S",
 			function()
 				require("flash").treesitter()
 			end,
+			mode = { "n", "x", "o" },
 			desc = "Flash Treesitter",
+		},
+		{
+			"f",
+			function()
+				require("flash").jump({
+					search = {
+						forward = true,
+						wrap = false,
+						max_length = 1,
+					},
+					jump = {
+						offset = 0,
+					},
+				})
+			end,
+			mode = { "n", "x", "o" },
+			desc = "Flash f",
+		},
+		{
+			"F",
+			function()
+				require("flash").jump({
+					search = {
+						forward = false,
+						wrap = false,
+						max_length = 1,
+					},
+					jump = {
+						offset = 0,
+					},
+				})
+			end,
+			mode = { "n", "x", "o" },
+			desc = "Flash F",
+		},
+		{
+			"t",
+			function()
+				require("flash").jump({
+					search = {
+						forward = true,
+						wrap = false,
+						max_length = 1,
+					},
+					jump = {
+						offset = 1,
+					},
+				})
+			end,
+			mode = { "n", "x", "o" },
+			desc = "Flash t",
+		},
+		{
+			"T",
+			function()
+				require("flash").jump({
+					search = {
+						forward = true,
+						wrap = false,
+						max_length = 1,
+					},
+					jump = {
+						offset = 1,
+					},
+				})
+			end,
+			mode = { "n", "x", "o" },
+			desc = "Flash T",
+		},
+	},
+	opts = {
+		char = {
+			enabled = false,
 		},
 	},
 }

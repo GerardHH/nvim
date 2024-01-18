6d72d1760d77f08cbb0dd3a4fd00521eaad563d4
Gerard Heshusius <gHeshusius@lely.com>
2024-01-18 13:48:13 +0100

    nvim-spider: Don't skip insignificant punctuations

--- lua/plugins/nvim-spider.lua
+++ lua/plugins/nvim-spider.lua
@@ -1,8 +1,12 @@
 return {
 	"chrisgrieser/nvim-spider",
+	lazy = true,
 	keys = {
 		{ "w", "<CMD>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" }, desc = "Spider-w" },
 		{ "e", "<CMD>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" }, desc = "Spider-e" },
 		{ "b", "<CMD>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" }, desc = "Spider-b" },
+	},
+	opts = {
+		skipInsignificantPunctuation = false, -- Don't skip to over to white space surrounded words
 	},
 }

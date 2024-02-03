1377c57e2459c0eb70a41dd37f3b0303d3982fcd
Gerard Heshusius <gHeshusius@lely.com>
2024-02-03 08:56:58 +0100

    rainbow-delimiter: Fix collered indentation

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -41,8 +41,6 @@ return {
 			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
 			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 		end)
-		require("ibl").setup({ scope = { highlight = highlight } })
-
-		hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+		require("ibl").setup({ indent = { highlight = highlight } })
 	end,
 }

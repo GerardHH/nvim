6911f4214228ce80e508db8b1833fd90b88e537a
Gerard Heshusius <gHeshusius@lely.com>
2024-03-05 15:42:07 +0100

    nvim-cmp: Use ghost text

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -24,6 +24,9 @@ return {
 
 			---@diagnostic disable:missing-fields
 			cmp.setup({
+				experimental = {
+					ghost_text = true,
+				},
 				window = {
 					documentation = cmp.config.window.bordered(),
 					completion = cmp.config.window.bordered(),
@@ -32,9 +35,7 @@ return {
 					completeopt = "menu,menuone,preview,noselect",
 				},
 				snippet = { -- configure how nvim-cmp interacts with snippet engine
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
+					expand = function(args) luasnip.lsp_expand(args.body) end,
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-k>"] = cmp.mapping.select_prev_item(),
@@ -117,8 +118,6 @@ return {
 			"rafamadriz/friendly-snippets",
 		},
 		lazy = true,
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
+		config = function() require("luasnip.loaders.from_vscode").lazy_load() end,
 	},
 }

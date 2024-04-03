0b0d5e1b616946138c7a7160e068d7a7b62caf3e
Gerard Heshusius <gHeshusius@lely.com>
2024-04-03 14:47:05 +0200

    trouble: Remove diagnostics keys (lspsaga handles this)

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -3,9 +3,6 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	lazy = true,
 	keys = {
-		-- Diagnostics
-		{ "<leader>dD", "<CMD>TroubleToggle workspace_diagnostics<CR>", desc = "Diagnostics workspace" },
-		{ "<leader>dd", "<CMD>TroubleToggle document_diagnostics<CR>", desc = "Diagnostics document" },
 		-- View
 		{ "<leader>vt", "<CMD>TroubleToggle<CR>", desc = "View Trouble toggle" },
 	},

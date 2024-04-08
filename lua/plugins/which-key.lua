f6be7de2dc499d07b0bee7221a4c4e5878cf6b2c
Gerard Heshusius <gHeshusius@lely.com>
2024-04-08 19:16:19 +0200

    which-key: Clean up

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -22,19 +22,14 @@ return {
 			b = { name = "Buffer" },
 			c = { name = "Comment" },
 			f = { name = "Find" },
-			g = {
-				name = "Git",
-				h = { name = "Hunk" },
-				t = { name = "Toggle" },
-			},
+			g = { name = "Git", h = { name = "Hunk" }, t = { name = "Toggle" } },
 			h = { name = "Highlight" },
-			j = { name = "Jump" },
 			l = { name = "LSP", d = { name = "Diagnostic" }, g = { name = "Go to" }, p = { name = "Peek" } },
 			m = { name = "Bookmark" },
 			r = { name = "Recorder" },
 			s = { name = "Surround" },
 			t = { name = "Telescope" },
-			v = { name = "View", S = { name = "Spectre (search&replace)" }, s = { name = "Split" } },
+			v = { name = "View", s = { name = "Split" } },
 		}, { mode = { "n", "v", "o", "x" }, prefix = "<leader>" })
 	end,
 }

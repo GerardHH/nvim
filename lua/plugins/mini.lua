4372854963928a3939c43814e9d6d44a8b71c267
Gerard Heshusius <gHeshusius@lely.com>
2024-05-11 00:01:16 +0200

    mini.jump & mini.jump2d: Use these instead of hop

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,4 +1,53 @@
+local jump_color = require("catppuccin.palettes.mocha").sky
+
 return {
+	{
+		"echasnovski/mini.jump",
+		version = "*",
+		lazy = true,
+		keys = {
+			{ "f", mode = { "n", "x", "o" }, desc = "Find char forward or continue forwards. Ends cursor on result" },
+			{
+				"F",
+				mode = { "n", "x", "o" },
+				desc = "Find char backwards or continue backwards. Ends cursor on result",
+			},
+			{
+				"t",
+				mode = { "n", "x", "o" },
+				desc = "Find char forward or continue forwards. Ends cursor before result",
+			},
+			{
+				"T",
+				mode = { "n", "x", "o" },
+				desc = "Find char backwards or continue backwards. Ends cursor before result",
+			},
+		},
+		opts = {},
+		config = function(pkg, opts)
+			require(pkg.name).setup(opts)
+			vim.cmd(string.format("highlight MiniJump cterm=standout gui=standout guifg=%s guibg=NONE", jump_color))
+		end,
+	},
+	{
+		"echasnovski/mini.jump2d",
+		version = "*",
+		lazy = true,
+		keys = {
+			"<CR>",
+			mode = { "n", "x", "o" },
+			desc = "Jump to any word/line (mini.jump2d)",
+		},
+		opts = {
+			view = {
+				dim = true,
+			},
+		},
+		config = function(pkg, opts)
+			require(pkg.name).setup(opts)
+			vim.cmd(string.format("highlight MiniJump2dSpot cterm=bold gui=bold guifg=%s guibg=NONE", jump_color))
+		end,
+	},
 	{
 		"echasnovski/mini.move",
 		version = "*",
